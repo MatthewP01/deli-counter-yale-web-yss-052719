36a0632bd6b3d3b221bb9632e6ab4beb4f8c2cf8
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-19 17:48:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     katz_deli.each do |line|
-      puts "The line is currently #{line_counter}. #{katz_deli[i]}"
+      puts "The line is currently #{line_counter}. #{line}"
     end
   end
 end
