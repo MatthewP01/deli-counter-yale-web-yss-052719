b30d1f8703a7f5dcf85e712a7ca4fae960101726
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-19 18:05:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
   line_total = []
   line_counter = 1
   katz_deli.each do |x|
-    counter =  "The line is currently #{line_counter}. #{x}"
+    counter =  "#{line_counter}. #{x}"
     # puts "The line is currently #{line_counter}. #{i}"
     line_total << counter
     line_counter += 1
